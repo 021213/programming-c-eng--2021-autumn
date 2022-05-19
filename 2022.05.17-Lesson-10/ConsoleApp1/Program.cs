154de3bb269bc8bd0ecc9f25146664b522f7bc70
021213 <st021213@spbu.ru>
2022-05-19 11:08:21 +0300

    C# intro

---
+++ 2022.05.17-Lesson-10/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int a = int.Parse(input[0]);
+            int.TryParse(input[1], out int b);
+
+            long c = (long)b + a;
+            Console.WriteLine(c);
+
+            //printf();
+            Console.WriteLine("{0}+{1}={2}", a, b, a + b);
+            var fstr = $"{a}+{b}={a + b}";
+            Console.WriteLine(fstr);
+        }
+    }
+}
---
+++ 2022.05.17-Lesson-10/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void changeArray(int[] a)
+        {
+            for (int i = 0; i < a.Length; ++i)
+            {
+                a[i] = a.Length - i;
+            }
+        }
+        static void printArray(int[] a)
+        {
+            for (int i = 0; i < a.Length; ++i)
+            {
+                Console.Write($"{a[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            int[] a = new int[10];
+            for(int i = 0; i < a.Length; ++i)
+            {
+                a[i] = i;
+            }
+            printArray(a);
+            changeArray(a);
+            printArray(a);
+        }
+    }
+}
